fa1a49970385d97dad88a69369b3ee442da4b2c1
diptimhabadi351 <mhabadidipti@gmail.com>
2024-04-02 16:08:50 +0530

    affiliate-dashblard

---
+++ affiliate-dashboard/component/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <base href="../">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>Trader's Circuit</title>
+    <link rel="icon" type="image/x-icon" href="../src/assets/img/seasons_logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    <link href="../layouts/css/light/loader.css" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="../src/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../src/plugins/src/apex/apexcharts.css" rel="stylesheet" type="text/css">
+    <link href="../src/assets/css/light/dashboard/dash_1.css" rel="stylesheet" type="text/css" />
+    <link href="../src/assets/css/light/custom.css" rel="stylesheet" type="text/css">
+    <link href="../src/assets/css/light/main.css" rel="stylesheet" type="text/css">
+    <link href="../src/assets/css/light/scrollspyNav.css" rel="stylesheet" type="text/css">
+    <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css"/>
+    <link href="../src/assets/css/light/components/list-group.css" rel="stylesheet" type="text/css">
+    <link href="../src/assets/css/light/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="../src/plugins/src/tagify/tagify.css">
+    <link rel="stylesheet" type="text/css" href="../src/plugins/css/light/tagify/custom-tagify.css">
+    <link rel="stylesheet" href="../src/plugins/src/filepond/filepond.min.css">
+    <link rel="stylesheet" href="../src/plugins/src/filepond/FilePondPluginImagePreview.min.css">
+    <link href="../src/plugins/css/light/filepond/custom-filepond.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="../src/plugins/src/table/datatable/datatables.css">
+    <link rel="sty lesheet" type="text/css" href="../src/plugins/css/light/table/datatable/dt-global_style.css">
+    <link rel="stylesheet" type="text/css" href="../src/plugins/css/light/table/datatable/custom_dt_custom.css">
+
+    <!-- added by abhishek -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../src/plugins/src/table/datatable/datatables.css">
+<link rel="stylesheet" type="text/css" href="../src/plugins/css//light/table/datatable/dt-global_style.css">
+
+</head>
+
+<body class="layout-boxed">
